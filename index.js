40057e164616c9b38c263919606bb5d7f036a36f
Brian Mulindi <mulindijrn@gmail.com>
2023-09-15 23:19:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0){
+        dodger.style.left = `${left - 3}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10)
+
+    if (left + dodger.clientWidth < window.innerWidth){
+        dodger.style.left = `${left + 3}px`;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }else if (event.key === "ArrowRight"){
+        moveDodgerRight();
+    }   
+}); 
+
+
